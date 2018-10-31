39ef464a7e6c139defdb94aa2ddf80aa7c99e06e
Justin Healy <justin.healy@bamtechmedia.com>
2018-10-31 15:19:02 -0700

    def inputs() works up till "Lets Play"

---
+++ Python/PracticePython/Lottery.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#"""TODO: Make a game that simulates the lottery. Allow the user to pick 5
+# numbers between 1 and 70 and 1 Mega Ball number betwen 1 and 25. Allow the
+# user to randomize the selection if desired. See if these numbers match 6
+# randomly generated numbers with the same stipulations. Set winning prizes
+# based on the Mega Millions prize chart."""
+
+
+ player_set = []
+ winning_set = []
+ open_game = inputs("\nWould you like to play the lottery?\n ")
+
+def inputs():
+    game_options = ['yes', 'no', 'exit']
+    while True:
+        open_game=str(input("\nWould you like to play the lottery?\n "))
+        if open_game.lower() not in game_options:
+            print("\n\nPlease type 'yes', 'no', or 'exit'. ")
+            continue
+        else:
+            break
+    if open_game.lower() in ['no', 'exit']:
+        print("\n\nCome back to play the lottery anytime! \n")
+    elif open_game.lower() == 'yes':
+        print("Lets play! \n")
+    style_options = ['choose', 'random']
+    while True:
+        stlye=str(input("Would you like to choose your numbers or randomly generate them? (choose/random) \n"))
+        if style.lower() not in style_options:
+            print("Please type 'choose', or 'random'. \n")
+            continue
+        else:
+            break
+    if style_options.lower() is 'choose':
+        while True:
+            num1, num2, num3, num4, num5 = [int(x) for x in input("Please pick 5 numbers between 1-70. \n").split()]
+
+
+
+
+
+ def main():
+     print("\nWelcome to the Mega Millions game!")
+     inputs()
