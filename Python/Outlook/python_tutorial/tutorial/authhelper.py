5365a3131a66e12c7aed812c9b3dee3adf69fc6c
Justin Healy <justin.healy@bamtechmedia.com>
2018-11-06 08:51:17 -0800

    Working acces token grab

--- Python/Outlook/python_tutorial/tutorial/authhelper.py
+++ Python/Outlook/python_tutorial/tutorial/authhelper.py
@@ -20,6 +20,7 @@ token_url = '{0}{1}'.format(authority, '/common/oauth2/v2.0/token')
 
 # The scopes required by the app
 scopes = [ 'openid',
+           'offline_access',
            'User.Read',
            'Mail.Read' ]
 
@@ -51,3 +52,46 @@ def get_token_from_code(auth_code, redirect_uri):
     return r.json()
   except:
     return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
+
+def get_token_from_refresh_token(refresh_token, redirect_uri):
+  # Build the post form for the token request
+  post_data = { 'grant_type': 'refresh_token',
+                'refresh_token': refresh_token,
+                'redirect_uri': redirect_uri,
+                'scope': ' '.join(str(i) for i in scopes),
+                'client_id': client_id,
+                'client_secret': client_secret
+              }
+
+  r = requests.post(token_url, data = post_data)
+
+  try:
+    return r.json()
+  except:
+    return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
+
+def get_access_token(request, redirect_uri):
+  current_token = request.session['access_token']
+  expiration = request.session['token_expires']
+  now = int(time.time())
+  if (current_token and now < expiration):
+    # Token still valid
+    return current_token
+  else:
+    # Token expired
+    refresh_token = request.session['refresh_token']
+    new_tokens = get_token_from_refresh_token(refresh_token, redirect_uri)
+
+    # Update session
+    # expires_in is in seconds
+    # Get current timestamp (seconds since Unix Epoch) and
+    # add expires_in to get expiration time
+    # Subtract 5 minutes to allow for clock differences
+    expiration = int(time.time()) + new_tokens['expires_in'] - 300
+
+    # Save the token in the session
+    request.session['access_token'] = new_tokens['access_token']
+    request.session['refresh_token'] = new_tokens['refresh_token']
+    request.session['token_expires'] = expiration
+
+    return new_tokens['access_token']
--- Python/Outlook/python_tutorial/tutorial/views.py
+++ Python/Outlook/python_tutorial/tutorial/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from tutorial.authhelper import get_signin_url
+from tutorial.authhelper import get_signin_url, get_token_from_code, get_access_token
+import time
 
 # Create your views here.
 
@@ -12,7 +14,7 @@ def home(request):
 
 # Add import statement to include new function
 from tutorial.outlookservice import get_me
-from tutorial.authhelper import get_token_from_code
+from tutorial.authhelper import get_token_from_code, get_access_token
 
 def gettoken(request):
   auth_code = request.GET['code']
@@ -20,7 +22,17 @@ def gettoken(request):
   token = get_token_from_code(auth_code, redirect_uri)
   access_token = token['access_token']
   user = get_me(access_token)
+  refresh_token = token['refresh_token']
+  expires_in = token['expires_in']
+
+  # expires_in is in seconds
+  # Get current timestamp (seconds since Unix Epoch) and
+  # add expires_in to get expiration time
+  # Subtract 5 minutes to allow for clock differences
+  expiration = int(time.time()) + expires_in - 300
 
   # Save the token in the session
   request.session['access_token'] = access_token
+  request.session['refresh_token'] = refresh_token
+  request.session['token_expires'] = expiration
   return HttpResponse('User: {0}, Access token: {1}'.format(user['displayName'], access_token))
